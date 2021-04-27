565e2b7e9452b4427fd5b4a1e605d32af34ee5f7
Erik Turøy Midtun <eriktm@smint.no>
2021-04-27 10:34:52 +0200

    turn voicerecognizer into state machine
    Co-authored-by: Wictor Zhao <wictor_x@hotmail.com>

--- src/record_voice.py
+++ src/record_voice.py
@@ -1,62 +1,88 @@
 import speech_recognition as sr
 import time
+class VoiceRecognizer:
+    def __init__(self, device):
+        self.over = False
+        self.r = sr.Recognizer()
+        self.mic = sr.Microphone(device_index=1)
+        self.device_driver = device_driver
 
-def recognize_speech_from_mic(recognizer, mic):
-  if not isinstance(recognizer, sr.Recognizer):
-    raise TypeError('`recognizer` must be `Recognizer` instance')
+        t0 = {'source': 'initial'
+              'target': 'off'}
+        t1 = {'trigger': 'on'
+              'source': 'recognizer_off'
+              'target': 'recognizer_on',
+              'effect': 'check_for_word(*)'}
+        t2 = {'trigger': 'off'
+              'source': 'recognizer_on'
+              'target': 'recognizer_off'}
 
-  if not isinstance(mic, sr.Microphone):
-    raise TypeError('`microphone` must be a `Microphone` instance')
+        recognizer_off = {
+            'name': 'recognizer_off',
+            'entry': 'turn_off'
+        }
+        recognizer_on = {
+            'name': 'recognizer_on',
+            'entry': 'check_for_word()'
+        }
 
-  with mic as source:
-    recognizer.adjust_for_ambient_noise(source)
-    audio = recognizer.listen(source)
-    # r.adjust_for_ambient_noise(source, duration=0.5)
+        self.machine = stmpy.Machine(name="voice_recognizer" transitions=[t0, t1, t2],
+                                     states=[off, recognizer_off, recognizer_on])
 
-  response = {
-    "success": True,
-    "error": None,
-    "transcription": None
-  }
+    def recognize_speech_from_mic(self, recognizer, mic):
+        if not isinstance(recognizer, sr.Recognizer):
+            raise TypeError('`recognizer` must be `Recognizer` instance')
 
-  try:
-    response["transcription"] = recognizer.recognize_google(audio)
-  except sr.RequestError:
-      # API was unreachable or unresponsive
-      response["success"] = False
-      response["error"] = "API unavailable"
-  except sr.UnknownValueError:
-      # speech was unintelligible
-      response["error"] = "Unable to recognize speech"
+        if not isinstance(mic, sr.Microphone):
+            raise TypeError('`microphone` must be a `Microphone` instance')
 
-  return response
+        with mic as source:
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source)
+            # r.adjust_for_ambient_noise(source, duration=0.5)
 
+        response = {
+            "success": True,
+            "error": None,
+            "transcription": None
+        }
 
-if __name__ == "__main__":
-  r = sr.Recognizer()
-  mic = sr.Microphone(device_index=1)
-  print('Say something')
-  time.sleep(1)
-  PROMPT_LIMIT = 2
-  over = False
+        try:
+            response["transcription"] = recognizer.recognize_google(audio)
+        except sr.RequestError:
+            # API was unreachable or unresponsive
+            response["success"] = False
+            response["error"] = "API unavailable"
+        except sr.UnknownValueError:
+            # speech was unintelligible
+            response["error"] = "Unable to recognize speech"
 
-  while not over:
-    for i in range(PROMPT_LIMIT):
-      print('Now')
-      output = recognize_speech_from_mic(r, mic)
-      if (output['transcription']):
-        break
-      if not output['success']:
-        break
-      print("Didnt catch end word. Try again")
+        return response
 
-    if output['error']:
-      print("ERROR: {}".format(output["error"]))
+    def check_for_word(self, word):
+        print('Say something')
+        time.sleep(1)
+        PROMPT_LIMIT = 2
+        while not over:
+            for i in range(PROMPT_LIMIT):
+                print('Now')
+                output = recognize_speech_from_mic(self.r, self.mic)
+                if output['transcription']:
+                    break
+                if not output['success']:
+                    break
+                print("Didnt catch end word. Try again")
 
-    print("You said: {}".format(output["transcription"]))
-    end_word = output["transcription"].lower().split()[-1] == "over"
-    if end_word:
-      over = True
-      break
-    else:
-      print("Not ended yet")
+            if output['error']:
+                print("ERROR: {}".format(output["error"]))
+
+            print("You said: {}".format(output["transcription"]))
+            transcript = output["transcription"].lower()
+            if word.lower() in transcript:
+                self.turn_off()
+                break
+            else:
+                print("Not ended yet")
+
+    def turn_off(self):
+        self.over = True
