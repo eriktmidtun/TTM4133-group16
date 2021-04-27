6866c13248f6218da64c467ad86cdaae3d5206e6
Erik Turøy Midtun <eriktm@smint.no>
2021-04-27 12:36:38 +0200

    separate device stm from mqtt logic

--- src/device.py
+++ src/device.py
@@ -1,6 +1,7 @@
 import stmpy
 import paho.mqtt.client as mqtt
 from playsound import playsound
+import json
 
 MQTT_BROKER = 'mqtt.item.ntnu.no'
 MQTT_PORT = 1883
@@ -9,11 +10,12 @@ MQTT_TOPIC_CHANNEL_BASE = 'ttm4115/team_16/channel/'
 MQTT_TOPIC_OUTPUT = 'ttm4115/team_16/answer'
 
 
-class Device(object):
+class DeviceLogic(object):
 
-    def __init__(self, mqtt_client):
-        self.mqtt_client = mqtt_client
+    def __init__(self, component):
+        self.component = component
         self.channel = None
+        self.channel_available = False
 
         """ transitions """
         t0 = {
@@ -37,7 +39,7 @@ class Device(object):
             "trigger": "subscribe_channel",
             "source": "no_channel",
             "target": "idle",
-            "effect": "change_channel",
+            "effect": "change_channel(*)",
         }
 
         t4 = {
@@ -45,66 +47,56 @@ class Device(object):
             "source": "idle",
             "target": "no_channel",
             "effect": "unsubscribe_channel",
+
         }
 
         t5 = {
             "trigger": "button_in",
             "source": "idle",
-            "target": "reserve_channel_button",
+            "target": "reserve_channel",
+            "effect": "voice_recognizer('off')",
         }
 
         t6 = {
             "trigger": "channel_unavailable",
-            "source": "reserve_channel_button",
+            "source": "reserve_channel",
             "target": "idle",
             "effect": "play_unavailable_sound",
         }
 
         t7 = {
             "trigger": "channel_reserved",
-            "source": "reserve_channel_button",
-            "target": "speaking_button",
+            "source": "reserve_channel",
+            "target": "speaking",
         }
 
         t8 = {
             "trigger": "button_out",
-            "source": "speaking_button",
+            "source": "speaking",
             "target": "idle",
         }
 
         t9 = {
             "trigger": "over",
-            "source": "speaking_voice",
+            "source": "speaking",
             "target": "idle",
         }
 
         t10 = {
-            "trigger": "channel_reserved",
-            "source": "reserve_channel_voice",
-            "target": "speaking_voice",
-        }
-
-        t11 = {
             "trigger": "wake_word",
             "source": "idle",
             "target": "reserve_channel_voice",
+            "effect": "voice_recognizer('end')",
         }
 
-        t12 = {
-            "trigger": "channel_unavailable",
-            "source": "reserve_channel_voice",
-            "target": "idle",
-            "effect": "play_unavailable_sound",
-        }
-
-        t13 = {
+        t11 = {
             "trigger": "change_channel",
             "source": "idle",
             "target": "idle",
             "effect": "change_channel",
         }
 
-        t14 = {
+        t12 = {
             "trigger": "switch_off",
             "source": "idle",
             "target": "off",
@@ -116,23 +108,17 @@ class Device(object):
         no_channel = {'name': 'no_channel'}
 
         idle = {'name': 'idle',
-                'entry': 'receiver(on)'}
+                'entry': 'receiver(on);voice_recognizer("wake")'}
 
-        reserve_channel_voice = {'name': 'reserve_channel_voice',
-                                 'entry': 'channel_availability(); reserve_channel()'}
+        reserve_channel = {'name': 'reserve_channel',
+                           'entry': 'channel_availability(); reserve_channel()'}
 
-        reserve_channel_button = {'name': 'reserve_channel_buttton',
-                                  'entry': 'channel_availability(); reserve_channel()'}
-        speaking_voice = {'name': 'speaking_voice',
-                          'entry': 'receiver(off);start_stream_audio();',
-                          'exit': 'stop_stream_audio();release_channel();ack_timout("listen")'}
+        speaking = {'name': 'speaking',
+                    'entry': 'receiver(off);start_stream_audio();',
+                    'exit': 'stop_stream_audio();release_channel();ack_timout("listen")'}
 
-        speaking_button = {'name': 'speaking_button',
-                           'entry': 'receiver(off);start_stream_audio();',
-                           'exit': 'stop_stream_audio();release_channel();ack_timout("listen")'}
-
-        self.stm = stmpy.Machine(name="Device" transitions=[t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14],
-                                 states=[off, no_channel, idle, reserve_channel_voice, reserve_channel_button, speaking_voice, speaking_button])
+        self.machine = stmpy.Machine(name="Device" transitions=[t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12],
+                                     states=[off, no_channel, idle, reserve_channel, speaking])
 
     def change_channel(self):
         new_channel = 1  # TODO hent channel fra UI
@@ -147,11 +133,20 @@ class Device(object):
     def unsubscribe_channel(self):
         channel_topic = MQTT_TOPIC_CHANNEL_BASE + str(self.channel) + "/#"
         self.mqtt_client.unsubscribe(channel_topic)
-        
+
     def channel_availability(self):
-        pass
+        reserve_topic = MQTT_TOPIC_CHANNEL_BASE + \
+            str(self.channel) + "/reserve"
+        if self.channel_unavailable:
+            self.machine.send('channel_unavailable')
+            return False
+        return True
 
     def reserve_channel(self):
+        reserve_topic = MQTT_TOPIC_CHANNEL_BASE + \
+            str(self.channel) + "/reserve"
+        if self.channel_availability()
+        self.mqtt_client.publish(reserve_topic, payload="")
         pass
 
     def release_channel(self):
@@ -167,7 +162,39 @@ class Device(object):
         pass
 
     def ack_timout(self, message):
-        pass
+        if message != "listen"
+        return  # Error
+        self.driver.send("")
+
+    def voice_recognizer(self, message):
+        self.driver.send("")
 
     def play_unavailable_sound(self):
         playsound("./assets/audio/unavailable_sound.mp3")
+
+
+class Device(object):
+
+    def __init__(self, mqtt_client, driver):
+        self.mqtt_client = mqtt_client
+        self.mqtt_client.on_connect = self.on_connect
+        self.mqtt_client.on_message = self.on_message
+        self.mqtt_client.connect(MQTT_BROKER, MQTT_PORT)
+        self.driver = driver
+        self.stm = DeviceLogic(self)
+        driver.add_machine(self.stm.machine)
+
+    def on_connect(self):
+        print("connected")
+
+    def on_message(self, client, userdata, msg):
+        try:
+            payload = json.loads(msg.payload.decode("utf-8"))
+        except Exception as err:
+            print('Message sent to topic {} had no valid JSON. Message ignored. {}'.format(
+                msg.topic, err))
+            return
+
+    def publish_message(self, data):
+        payload = json.dumps(data)
+        self.mqtt_client.publish(MQTT_TOPIC_INPUT, payload=payload, qos=2)
