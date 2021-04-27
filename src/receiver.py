e7b7c0277bfe06d2f9326af961751a150755de02
Erik Turøy Midtun <eriktm@smint.no>
2021-04-27 12:36:58 +0200

    add receiver and recorder code

--- src/receiver.py
+++ src/receiver.py
@@ -1,5 +1,6 @@
 from stmpy import Driver, Machine
-
+import pyaudio
+import wave
 
 class Receiver:
     def __init__(self, mqtt_client):
@@ -36,15 +37,15 @@ class Receiver:
               'target': 'receiver_on'}
 
         # DEFINING THE STATES
-        receiver_off{
+        receiver_off = {
             'name': 'receiver_off',
         }
 
-        receiver_on{
+        receiver_on = {
             'name': 'receiver_on'
         }
 
-        play_voice_message{
+        play_voice_message = {
             'name': 'play_voice_message',
             'entry': 'play_message'
         }
@@ -53,7 +54,35 @@ class Receiver:
                            states=[receiver_on, receiver_off, play_voice_message])
 
     def play_message(self):
-        pass
+        filename = 'output.wav'
+
+        # Set chunk size of 1024 samples per data frame
+        chunk = 1024
+
+        # Open the sound file
+        wf = wave.open(filename, 'rb')
+
+        # Create an interface to PortAudio
+        p = pyaudio.PyAudio()
+
+        # Open a .Stream object to write the WAV file to
+        # 'output = True' indicates that the sound will be played rather than recorded
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+
+        # Read data in chunks
+        data = wf.readframes(chunk)
+
+        # Play the sound by writing the audio data to the stream
+        while data != '':
+            stream.write(data)
+            data = wf.readframes(chunk)
+
+        # Close and terminate the stream
+        stream.close()
+        p.terminate()
 
     def play_error_sound(self):
-        pass
+        playsound("./assets/audio/error_sound.mp3")
---
+++ src/recorder.py
@@ -0,0 +1,77 @@
+from stmpy import Machine, Driver
+from os import system
+import os
+import time
+
+import pyaudio
+import wave
+
+        
+class Recorder:
+    def __init__(self):
+        self.recording = False
+        self.chunk = 1024  
+        self.sample_format = pyaudio.paInt16  
+        self.channels = 2
+        self.fs = 44100  
+        self.filename = "output.wav"
+        self.p = pyaudio.PyAudio() 
+        
+    def record(self):
+        stream = self.p.open(format=self.sample_format,
+                channels=self.channels,
+                rate=self.fs,
+                frames_per_buffer=self.chunk,
+                input=True)
+        self.frames = []  # Initialize array to store frames
+        # Store data in chunks for 3 seconds
+        self.recording = True
+        while self.recording:
+            data = stream.read(self.chunk)
+            self.frames.append(data)
+        print("done recording")
+        # Stop and close the stream 
+        stream.stop_stream()
+        stream.close()
+        # Terminate the PortAudio interface
+        self.p.terminate()
+        
+    def stop(self):
+        print("stop")
+        self.recording = False
+    
+    def process(self):
+        print("processing")
+        # Save the recorded data as a WAV file
+        wf = wave.open(self.filename, 'wb')
+        wf.setnchannels(self.channels)
+        wf.setsampwidth(self.p.get_sample_size(self.sample_format))
+        wf.setframerate(self.fs)
+        wf.writeframes(b''.join(self.frames))
+        wf.close()
+
+recorder = Recorder()
+        
+t0 = {'source': 'initial', 'target': 'ready'}
+t1 = {'trigger': 'start', 'source': 'ready', 'target': 'recording'}
+t2 = {'trigger': 'done', 'source': 'recording', 'target': 'processing'}
+t3 = {'trigger': 'done', 'source': 'processing', 'target': 'ready'}
+
+s_recording = {'name': 'recording', 'do': 'record()', "stop": "stop()"}
+s_processing = {'name': 'processing', 'do': 'process()'}
+
+stm = Machine(name='stm', transitions=[t0, t1, t2, t3], states=[s_recording, s_processing], obj=recorder)
+recorder.stm = stm
+
+driver = Driver()
+driver.add_machine(stm)
+driver.start()
+
+print("driver started")
+
+driver.send('start', 'stm')
+print("sent start, now waiting for a 5 seconds long recording")
+time.sleep(5)
+print("wait is over")
+driver.send('stop', 'stm')
+print("sent stop")
